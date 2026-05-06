769baa15e30184b44579dc2b561bc30e46359955
Chris Foster <chrisfoster186@googlemail.com>
2026-05-06 19:50:03 +0100

    Adds return from BiL scenarios

---
+++ stories/apprenticeship-bil-completion-after-qp/payloads/02-get-learning-key.sql
@@ -0,0 +1,4 @@
+SELECT TOP 1 al.[Key] AS LearningKey
+FROM [ApprenticeshipLearning] al
+INNER JOIN [ApprenticeshipEpisode] ae ON ae.[LearningKey] = al.[Key]
+WHERE ae.[ApprovalsApprenticeshipId] = 700006
